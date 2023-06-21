6991ceace4ad05f1636a3c9024bc45e66a3549c4
BananaLF <864685021@qq.com>
2023-06-22 00:51:30 +0800

    add brc30 test tool

--- coins/brc20/inscribe_test.go
+++ coins/brc20/inscribe_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/rpcclient"
+	"github.com/stretchr/testify/require"
 )
 
 func TestInscribe(t *testing.T) {
@@ -79,4 +81,91 @@ func TestInscribe(t *testing.T) {
 
 	txsBytes, _ := json.Marshal(txs)
 	t.Log(string(txsBytes))
+}
+
+func TestBrc30(t *testing.T) {
+	network := &chaincfg.RegressionNetParams
+	client := modeRpcClient()
+	defer client.Shutdown()
+
+	commitTxPrevOutputList := make([]*PrevOutput, 0)
+	commitTxPrevOutputList = append(commitTxPrevOutputList, &PrevOutput{
+		TxId:       "28d438e90349dcb15ea93d87fb0478602b8efbf7a2852861811ce242a9c54a82",
+		VOut:       0,
+		Amount:     5000000000,
+		Address:    "bcrt1qdk34rmzke023v04xxvpxz0fwauctsxk42ue2zj",
+		PrivateKey: "cUfpPiyCfuiAaLLKxSJMa6Js5f5gL1EHbWvXznxaLgqXmnWNtP1o",
+	})
+
+	inscriptionDataList := make([]InscriptionData, 0)
+	inscriptionDataList = append(inscriptionDataList, InscriptionData{
+		ContentType: "text/plain;charset=utf-8",
+		Body:        []byte(`{"p":"brc-20","op":"mint","tick":"xcvb","amt":"1000"}`),
+		RevealAddr:  "tb1qtsq9c4fje6qsmheql8gajwtrrdrs38kdzeersc",
+	})
+	inscriptionDataList = append(inscriptionDataList, InscriptionData{
+		ContentType: "text/plain;charset=utf-8",
+		Body:        []byte(`{"p":"brc-20","op":"mint","tick":"xcvb","amt":"1000"}`),
+		RevealAddr:  "2NF33rckfiQTiE5Guk5ufUdwms8PgmtnEdc",
+	})
+	inscriptionDataList = append(inscriptionDataList, InscriptionData{
+		ContentType: "text/plain;charset=utf-8",
+		Body:        []byte(`{"p":"brc-20","op":"mint","tick":"xcvb","amt":"1000"}`),
+		RevealAddr:  "mouQtmBWDS7JnT65Grj2tPzdSmGKJgRMhE",
+	})
+	inscriptionDataList = append(inscriptionDataList, InscriptionData{
+		ContentType: "text/plain;charset=utf-8",
+		Body:        []byte(`{"p":"brc-20","op":"mint","tick":"xcvb","amt":"1000"}`),
+		RevealAddr:  "tb1pklh8lqax5l7m2ycypptv2emc4gata2dy28svnwcp9u32wlkenvsspcvhsr",
+	})
+
+	request := &InscriptionRequest{
+		CommitTxPrevOutputList: commitTxPrevOutputList,
+		CommitFeeRate:          2,
+		RevealFeeRate:          2,
+		RevealOutValue:         546,
+		InscriptionDataList:    inscriptionDataList,
+		ChangeAddress:          "2NF33rckfiQTiE5Guk5ufUdwms8PgmtnEdc",
+	}
+
+	requestBytes, _ := json.Marshal(request)
+	log.Println(string(requestBytes))
+
+	tool, err := newInscriptionTool(network, request)
+	if err != nil {
+		t.Fatal("build inscribe error:", err.Error())
+	}
+
+	hehe, err := tool.getCommitTxHex()
+	require.NoError(t, err)
+	t.Log("commit tx: ", hehe)
+	//send commit tx
+	commitTXID, err := client.SendRawTransaction(tool.CommitTx, true)
+	if err != nil {
+		t.Fatal("send commit error:", err.Error(), "commit tx", *tool.CommitTx)
+	}
+	t.Log("Commit TXID:", commitTXID.String())
+
+	for i, _ := range tool.RevealTx {
+		revealTXID, err := client.SendRawTransaction(tool.RevealTx[i], true)
+		if err != nil {
+			t.Fatal("send reveal error:", err.Error(), "commit tx", tool.CommitTx)
+		}
+		t.Log("Reveal TXID:", revealTXID.String())
+	}
+}
+
+func modeRpcClient() *rpcclient.Client {
+	connCfg := &rpcclient.ConnConfig{
+		Host:         "<ip>:<port>",
+		User:         "<username>",
+		Pass:         "<password>",
+		HTTPPostMode: true,
+		DisableTLS:   true,
+	}
+	client, err := rpcclient.New(connCfg, nil)
+	if err != nil {
+		log.Fatalf("error creating client: %v", err)
+	}
+	return client
 }
